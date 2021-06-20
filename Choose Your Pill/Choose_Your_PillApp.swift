d958baed88d6d074f2aa301471242e55205af896
Caio <caiovr13@gmail.com>
2021-06-20 11:05:41 -0300

    Initial Commit

---
+++ Choose Your Pill/Choose_Your_PillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_Your_PillApp.swift
+//  Choose Your Pill
+//
+//  Created by Caio Fernandes on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Choose_Your_PillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
